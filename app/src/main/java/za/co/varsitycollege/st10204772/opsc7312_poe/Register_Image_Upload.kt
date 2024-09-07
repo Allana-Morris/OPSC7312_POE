30683feaf0af382541cb30c2bb2e37ff825eb7be
Allana-Morris <ST10204772@vcconnect.edu.za>
2024-09-07 13:58:15 +0200

    Changes and Register_Cell UI completed

--- app/src/main/java/za/co/varsitycollege/st10204772/opsc7312_poe/Register_Image_Upload.kt
+++ app/src/main/java/za/co/varsitycollege/st10204772/opsc7312_poe/Register_Image_Upload.kt
@@ -6,11 +6,11 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class login_otp : AppCompatActivity() {
+class Register_Image_Upload : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_login_otp)
+        setContentView(R.layout.activity_register_image_upload)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
