546eb4265bc2fcf1bd7549bd480858ec0724c60f
ST10030798 <ST10030798@vcconnect.edu.za>
2024-11-02 13:39:22 +0200

    regret

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,10 @@ android {
 }
 
 dependencies {
+    val room_version = "2.6.1"
+
+    implementation ("androidx.room:room-runtime:$room_version")
+    annotationProcessor ("androidx.room:room-compiler:$room_version")
 
     //CircleImageView Library
     implementation(libs.circleimageview)
